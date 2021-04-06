35c6aa6d5b55b3e030b6a01a7d9ba16e3066a4cc
Danny Hahn <dhahn219@gmail.com>
2021-04-05 19:08:12 -0500

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,6 @@ export class AppComponent {
  
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
- 
           let fetchedSatellites = data.satellites;
           for(let i = 0; i < fetchedSatellites.length; i++) {
             const satellite =  new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
